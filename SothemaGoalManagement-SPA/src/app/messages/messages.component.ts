60c099880b0222b6307647672ec27421514aa6f7
Elmehdi <eaitbrahim@gmail.com>
2019-09-06 11:37:44 +0100

    Fix strategy creation layout

--- SothemaGoalManagement-SPA/src/app/messages/messages.component.ts
+++ SothemaGoalManagement-SPA/src/app/messages/messages.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Message } from '../_models/message';
-import { Pagination, PaginatedResult } from '../_models/Pagination';
+import { Pagination, PaginatedResult } from '../_models/pagination';
 import { UserService } from '../_services/user.service';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
