5763ad0ea0363b1b1ec1e3326be3deeccf4b8b4f
Elmehdi <eaitbrahim@gmail.com>
2019-09-18 23:02:19 +0100

    Group axis poles by axis

--- SothemaGoalManagement-SPA/src/app/strategies/strategies.component.ts
+++ SothemaGoalManagement-SPA/src/app/strategies/strategies.component.ts
@@ -10,14 +10,29 @@ import { Strategy } from '../_models/strategy';
 })
 export class StrategiesComponent implements OnInit {
   strategies: Strategy[];
+  axisPolesGroupedByAxis = {};
+  keys: string[];
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.route.data.subscribe(data => {
       this.strategies = data['strategies'];
 
-      console.log('this.strategies: ', this.strategies);
+      this.strategies.forEach(strategy => {
+        this.axisPolesGroupedByAxis = this.groupBy(strategy.axisPoles, 'axisDescription');
+        this.keys = Object.keys(this.axisPolesGroupedByAxis);
+        console.log('axisPolesGroupedByAxis:', this.axisPolesGroupedByAxis);
+      });
+
+
     });
   }
+
+  groupBy(xs, key) {
+    return xs.reduce(function (rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
 
 }
